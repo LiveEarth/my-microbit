36b5ea0cd7999ac6800714ebbd3d9003ff9430f3
Georgi Uzunov <georgiuzunov87@abv.bg>
2022-04-12 17:22:03 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let bearing = 0
+for (let index = 0; index < 2; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+}
+RussianFont.showMessage("Здравей Макси ☺", 50)
+soundExpression.happy.play()
 basic.forever(function () {
-	
+    bearing = input.compassHeading()
+    if (bearing < 45 || bearing > 315) {
+        basic.showString("N")
+    } else {
+        basic.showString(" ")
+    }
 })
