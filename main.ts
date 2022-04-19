42f78067df7b1ae2685d350ecb23a9730c3e98ca
Georgi Uzunov <georgiuzunov87@abv.bg>
2022-04-19 12:23:46 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,27 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Duck)
-    basic.pause(5000)
+    dice = randint(0, 99 + 1)
+    basic.showNumber(dice)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.House)
     basic.pause(5000)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showIcon(IconNames.StickFigure)
-    basic.pause(5000)
+    compassdegre = input.compassHeading()
+    if (compassdegre < 45) {
+        RussianFont.showMessage("с", 50)
+    } else if (compassdegre < 135) {
+        RussianFont.showMessage("и", 50)
+    } else if (compassdegre < 225) {
+        RussianFont.showMessage("ю", 50)
+    } else if (compassdegre < 315) {
+        RussianFont.showMessage("з", 50)
+    } else {
+        RussianFont.showMessage("с", 50)
+    }
 })
-let bearing = 0
+let compassdegre = 0
+let dice = 0
 for (let index = 0; index < 1; index++) {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
@@ -19,12 +30,5 @@ for (let index = 0; index < 1; index++) {
 RussianFont.showMessage("☺", 50)
 soundExpression.happy.play()
 basic.forever(function () {
-    bearing = input.compassHeading()
-    if (bearing < 45 || bearing > 315) {
-        basic.showString("N")
-    } else {
-        led.plotBrightness(randint(0, 5), randint(0, 5), 255)
-        led.plotBrightness(randint(0, 5), randint(0, 5), 0)
-        basic.pause(200)
-    }
+	
 })
