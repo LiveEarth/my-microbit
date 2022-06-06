63378a80af762caa9a464b1f54ef66eee48a8458
Georgi Uzunov <georgiuzunov87@abv.bg>
2022-06-06 16:49:29 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -60,7 +60,7 @@ input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
 input.onButtonPressed(Button.A, function () {
     randomNUM1 = randint(1, 100)
     randomNUM2 = randint(1, 100)
-    for (let index2 = 0; index2 < 120; index2++) {
+    for (let index = 0; index < 120; index++) {
         led.plotBrightness(randint(0, 5), randint(0, 5), 255)
         basic.pause(25)
     }
@@ -92,7 +92,7 @@ input.onButtonPressed(Button.A, function () {
     20,
     1
     )
-    index = 0
+    index4 = 0
     for (let index = 0; index <= 180; index++) {
         OLED12864_I2C.showString(
         4,
@@ -101,6 +101,8 @@ input.onButtonPressed(Button.A, function () {
         1
         )
         if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            break;
+        } else if (input.logoIsPressed()) {
             break;
         }
         basic.pause(1000)
@@ -116,7 +118,7 @@ input.onButtonPressed(Button.B, function () {
         randomNUM2 = randint(1, 100)
     }
     if (randomNUM1 >= randomNUM2) {
-        for (let index2 = 0; index2 < 120; index2++) {
+        for (let index = 0; index < 120; index++) {
             led.plotBrightness(randint(0, 5), randint(0, 5), 255)
             basic.pause(25)
         }
@@ -149,15 +151,17 @@ input.onButtonPressed(Button.B, function () {
         1
         )
     }
-    index = 0
-    for (let index = 0; index <= 180; index++) {
+    index4 = 0
+    for (let index3 = 0; index3 <= 180; index3++) {
         OLED12864_I2C.showString(
         4,
         1,
-        "Time:" + (180 - index),
+        "Time:" + (180 - index3),
         1
         )
         if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            break;
+        } else if (input.logoIsPressed()) {
             break;
         }
         basic.pause(1000)
@@ -167,7 +171,7 @@ input.onButtonPressed(Button.B, function () {
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     OLED12864_I2C.clear()
-    for (let index2 = 0; index2 < 120; index2++) {
+    for (let index = 0; index < 120; index++) {
         led.plotBrightness(randint(0, 5), randint(0, 5), 255)
         basic.pause(25)
     }
@@ -183,13 +187,13 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     OLED12864_I2C.clear()
 })
 let picture = 0
-let index = 0
+let index4 = 0
 let resultMATH = 0
 let randomNUM2 = 0
 let randomNUM1 = 0
 let compassdegre = 0
 OLED12864_I2C.init(60)
-for (let index2 = 0; index2 < 2; index2++) {
+for (let index = 0; index < 2; index++) {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
     basic.showIcon(IconNames.SmallHeart)
@@ -219,7 +223,7 @@ basic.forever(function () {
         picture = randint(1, 5)
         if (picture == 1) {
             soundExpression.happy.play()
-            for (let index2 = 0; index2 < 3; index2++) {
+            for (let index = 0; index < 3; index++) {
                 basic.pause(1000)
                 basic.showLeds(`
                     . # . # .
@@ -241,7 +245,7 @@ basic.forever(function () {
             basic.clearScreen()
         } else if (picture == 2) {
             soundExpression.happy.play()
-            for (let index2 = 0; index2 < 3; index2++) {
+            for (let index = 0; index < 3; index++) {
                 basic.pause(1000)
                 basic.showLeds(`
                     . . # . .
@@ -263,7 +267,7 @@ basic.forever(function () {
             basic.clearScreen()
         } else if (picture == 3) {
             soundExpression.happy.play()
-            for (let index2 = 0; index2 < 3; index2++) {
+            for (let index = 0; index < 3; index++) {
                 basic.pause(1000)
                 basic.showLeds(`
                     # # . . .
@@ -285,7 +289,7 @@ basic.forever(function () {
             basic.clearScreen()
         } else if (picture == 4) {
             soundExpression.happy.play()
-            for (let index2 = 0; index2 < 3; index2++) {
+            for (let index = 0; index < 3; index++) {
                 basic.pause(1000)
                 basic.showLeds(`
                     # . # . .
@@ -307,7 +311,7 @@ basic.forever(function () {
             basic.clearScreen()
         } else {
             soundExpression.happy.play()
-            for (let index2 = 0; index2 < 3; index2++) {
+            for (let index = 0; index < 3; index++) {
                 basic.pause(1000)
                 basic.showLeds(`
                     . # # # .
